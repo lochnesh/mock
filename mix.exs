809dd9d845d0a82a1a759dcc3ba25ce295be7a5d
Skyler Nesheim <skyler@dwolla.com>
2014-06-18 16:15:09 -0500

    update meck

--- mix.exs
+++ mix.exs
@@ -7,20 +7,13 @@ defmodule Mock.Mixfile do
       source_url: "https://github.com/jjh42/mock",
       homepage_url: "http://jjh42.github.io/mock",
       version: "0.0.3",
-      env: [
-          dev: [deps: deps ++ dev_deps],
-          test: [deps: deps] ,
-          prod: [deps: deps]]]
+      deps: deps      ]
   end
 
   # Returns the list of dependencies in the format:
   # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    [ {:meck,"0.7.2", [github: "eproxus/meck", tag: "0.7.2"]}]
+    [ {:meck, github: "lochnesh/meck"}]
   end
 
-  # Dependencies only needed during development.
-  defp dev_deps do
-    [{ :docs_ghpages, github: "jjh42/docs_ghpages" }]
-  end
 end
