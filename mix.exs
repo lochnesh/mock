4426befa988db661415a0233e1cfe2e22203bee5
Skyler Nesheim <skyler@dwolla.com>
2014-06-19 08:40:48 -0500

    remove only test dep meck

--- mix.exs
+++ mix.exs
@@ -13,7 +13,7 @@ defmodule Mock.Mixfile do
   # Returns the list of dependencies in the format:
   # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    [{ :meck, github: "eproxus/meck", tag: "0.8.2", only: :test } ]
+    [{ :meck, github: "eproxus/meck", tag: "0.8.2" } ]
   end
 
 end
